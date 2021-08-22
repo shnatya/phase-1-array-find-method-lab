b6c8444b8443a1c5a4807fd3aa9c6f444e81d434
Anastasia Shatokhina <shnatya@gmail.com>
2021-08-22 16:06:10 -0700

    commit tree 08b01c2df39731393a0a69f24304fe5431c93211

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+/*function superbowlWin(item){
+        return item.result === "W";
+    
+}
 
+let x = record.find(superbowlWin)*/
+
+/*function superbowlWin(array){
+    let x = array.find(item => {
+        if(item.result === "W"){
+            return item;
+        }
+    } )
+    if(x){
+        return x.year;
+    }
+    else{
+        return x;
+    }
+    
+}*/
+
+function superbowlWin(array){
+    let x = array.find(item => item.result === "W")
+    if(x){
+        return x.year;
+    }
+    else{
+        return x;
+    }
+    
+}
+    
